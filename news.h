1daf1e232d2354770df5d51749898a8541b9174a
Kommendant <jonasd90@gmail.com>
2017-02-04 15:43:17 +0100

    added empty constructors

--- news.h
+++ news.h
@@ -3,20 +3,11 @@
 #include <ctime>
 #include <vector>
 #include <string>
+
 namespace news{
-    class Newsgroup{ 
-        public:
-            Newsgroup();
-            ~Newsgroup();
-            std::string name;
-            unsigned int id;
-            std::time_t created;
-            bool add(Article);
-            // returns a vector with articles listed in chronological order
-            std::vector<Article> to_list();
-    };
     class Article{
         public:
+            Article();
             Article(std::string, std::string, std::string);
             ~Article();
             std::string title;
@@ -24,6 +15,24 @@ namespace news{
             std::string content;
             unsigned int id;
             std::time_t created;
+    };
+    class Newsgroup{ 
+        public:
+            Newsgroup();
+            Newsgroup(std::string, unsigned int id, std::time_t);
+            ~Newsgroup();
+            std::string name;
+            unsigned int id;
+            std::time_t created;
+            bool add(Article);
+            // returns a vector with articles listed in chronological order
+            std::vector<Article> to_list();
+            /* delete an article
+             * @param: id of article to be deleted 
+             */
+            bool del(unsigned int id);
+        private:
+            std::vector<Article> articles;
     };
 
 }
