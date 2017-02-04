33df54bb9f628d88c679c475137f5e67dc72c527
Kommendant <jonasd90@gmail.com>
2017-02-04 15:13:58 +0100

    news header

---
+++ news.h
@@ -0,0 +1,29 @@
+#ifndef NEWS_H
+#define NEWS_H
+#include <ctime>
+#include <string>
+namespace news{
+    class Newsgroup{ 
+        public:
+            Newsgroup();
+            ~Newsgroup();
+            std::string name;
+            unsigned int id;
+            std::time_t created;
+            bool add(Article);
+            // returns a vector with articles listed in chronological order
+            std::vector<Article> to_list();
+    }
+    class Article{
+        public:
+            Article(std::string, std::string, std::string);
+            std::string title;
+            std::string author;
+            std::string content;
+            unsigned int id;
+            std::time_t created;
+    }
+
+}
+
+#endif
